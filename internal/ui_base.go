7c70860fc597b10e7fdd0f341cbbb864841a3c16
Juijote <Juijote@outlook.com>
2023-12-15 13:45:04 +0800

    up

--- internal/ui_base.go
+++ internal/ui_base.go
@@ -22,9 +22,10 @@ import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/dialog"
+	"fyne.io/fyne/v2/container/layout"
+	"fyne.io/fyne/v2/container/tab"
+	"fyne.io/fyne/v2/container/widget"
 	"fyne.io/fyne/v2/theme"
-	"fyne.io/fyne/v2/widget"
 )
 
 func makeUI() {
