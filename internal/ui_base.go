ac882ac33fc5fb3a5ac2719a9ae895b752147721
Juijote <Juijote@outlook.com>
2023-12-15 13:25:37 +0800

    up

--- internal/ui_base.go
+++ internal/ui_base.go
@@ -1,4 +1,4 @@
-﻿// CryoUtilities
+// CryoUtilities
 // Copyright (C) 2023 CryoByte33 and contributors to the CryoUtilities project
 
 // This program is free software: you can redistribute it and/or modify
